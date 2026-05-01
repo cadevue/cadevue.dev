2e3d7b8b2b5bb7367d0c9532cf7f5aaf5c0758d6
Addin Munawwar 🌧 <91366027+cadevue@users.noreply.github.com>
2026-05-02 00:20:57 +0700

    feat: install needed deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import expressiveCode from 'astro-expressive-code';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), expressiveCode(), pagefind()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});

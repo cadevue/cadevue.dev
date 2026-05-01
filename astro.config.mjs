3181a8ee4065fe4ed8fced905ce1f11c91f5fd09
Addin Munawwar 🌧 <91366027+cadevue@users.noreply.github.com>
2026-05-01 23:53:24 +0700

    feat: restart with astro v6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), sitemap()],
-  prefetch: {
-    defaultStrategy: 'viewport',
-    prefetchAll: true,
-  },
-  site: 'https://cadevue.com',
-});
+export default defineConfig({});
--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-const specialTags : {[key: string]: string} = {
-    'C#' : 'csharp'
-}
-
-export const labelToTagValue = (label: string) => {
-    return specialTags[label] || label.toLowerCase().replace(/\s/g, '-');
-}

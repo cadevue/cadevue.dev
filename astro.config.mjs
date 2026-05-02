e26e62ac6105d809444be26682ba725208165b41
Addin Munawwar 🌧 <91366027+cadevue@users.noreply.github.com>
2026-05-02 14:06:12 +0700

    feat: add minute read

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import expressiveCode from 'astro-expressive-code';
 import pagefind from 'astro-pagefind';
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap(), expressiveCode(), pagefind()],
+
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 
   redirects: {
     '/about-me': '/',
